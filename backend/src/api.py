a997853bd32ed929f25ed954127a53f7a70438e0
Pradeep Pyatalo Jayarama <pradeeppj@gmail.com>
2020-09-13 19:38:32 -0500

    Implement api logic and add authorization checks. Update UI to point to Auth0 configs.

--- backend/src/api.py
+++ backend/src/api.py
@@ -16,7 +16,7 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+#db_drop_and_create_all()
 
 ## ROUTES
 '''
@@ -27,6 +27,22 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    auth_header = request.headers.get('Authorization', None)
+    
+    if auth_header is None:
+        print('Authorization header is not set in the request.')
+        abort(401)
+
+    all_drinks = Drink.query.all()
+    if all_drinks is None or len(all_drinks) == 0:
+        abort(404)
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.short() for drink in all_drinks]
+    }), 200
 
 
 '''
@@ -37,6 +53,18 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    all_drinks = Drink.query.all()
+    if all_drinks is None or len(all_drinks) == 0:
+        abort(404)
+
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long() for drink in all_drinks]
+    }), 200
+
 
 
 '''
@@ -48,6 +76,32 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(payload):
+    request_body = request.get_json()
+
+    if not request_body:
+        print('There was no request body. Not a valid request')
+        abort(400)
+
+    title = request_body.get('title', None)
+    recipe = request_body.get('recipe', None)
+
+    if (title is None or recipe is None):
+        print("Request body missing Title or Recipe information required to create a drink")
+        abort(422)    
+
+    drink = Drink()
+    drink.title = title
+    drink.recipe = json.dumps([recipe])
+    
+    drink.insert()
+
+    return jsonify({
+        "success": True,
+        "drinks": [drink.long()]
+    })
 
 
 '''
@@ -61,6 +115,41 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(payload, id):
+    drink = Drink.query.get(id)
+
+    if drink is None:
+        print(f'There is no drink with id {drink_id}. Nothing to update')
+        abort(404)
+
+    request_body = request.get_json()
+
+    if not request_body:
+        print('There was no request body. Not a valid request')
+        abort(400)
+
+    title = request_body.get('title', None)
+    recipe = request_body.get('recipe', None)
+
+    try:
+        if title:
+            drink.title = title        
+        
+        if recipe:
+            drink.recipe = json.dumps([recipe])
+
+        drink.update()
+        
+    except Exception as e:
+        print(f'There was an exception trying to update drink {id}')
+        abort(422)
+
+    return jsonify({
+            "success": True,
+            "drinks": [drink.long()]
+        })  
 
 
 '''
@@ -73,6 +162,26 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    drink = Drink.query.get(id)
+
+    if drink is None:
+        print(f'There is no drink with id {drink_id}. Nothing to update')
+        abort(404)
+
+    try:
+        drink.delete()
+    except Exception as e:
+        print(f'There was an exception trying to delete drink {id}')
+        abort(422)
+
+    return jsonify({
+        "success": True,
+        "delete": id
+    })
+
 
 
 ## Error Handling
@@ -86,6 +195,23 @@ def unprocessable(error):
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource Not Found"
+    }), 404
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+    }), 400
+
 
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
@@ -108,3 +234,10 @@ def unprocessable(error):
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    return jsonify({
+        "success": False,
+        "code": ex.error['code'],
+        "description": ex.error['description']
+    }), ex.status_code
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -3,11 +3,12 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+from os import environ
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = environ.get('AUTH0_DOMAIN', 'dev-pjpk.us.auth0.com')
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = environ.get('API_AUDIENCE', 'coffeeshop')
 
 ## AuthError Exception
 '''
@@ -31,7 +32,34 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    # Retrieve the authorization header
+    auth_header = request.headers.get('Authorization', None)
+
+    # If auth header is not set in request, return error
+    if auth_header is None:
+        raise AuthError({
+            "code": "authorization_header_missing",
+            "description": "Authorization header is expected"
+        }, 401)
+
+    header_parts = auth_header.split(' ')
+
+    # validate Auth header has a bearer token
+    if len(header_parts) != 2 or not header_parts:
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "Authorization header must be in the format"
+                            " Bearer token"
+        }, 401)
+    elif header_parts[0].lower() != 'bearer':
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "Authorization header must start with"
+                            " Bearer"
+        }, 401)
+   
+    # Return the token value from the the header
+    return header_parts[1]
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -45,7 +73,16 @@ def get_token_auth_header():
     return true otherwise
 '''
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    if 'permissions' not in payload:
+        abort(400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'Unauthorized Action',
+            'description': 'Operation not allowed. Check your permissions.'
+        }, 401)
+
+    return True
 
 '''
 @TODO implement verify_decode_jwt(token) method
@@ -61,7 +98,60 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+
+    rsa_key = {}
+
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer=f'https://{AUTH0_DOMAIN}/'
+            )
+
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'token is expired'
+            }, 401)
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'incorrect claims,'
+                                'please check the audience adn issuer'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 401)
+
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find appropriate key'
+            }, 401)
 
 '''
 @TODO implement @requires_auth(permission) decorator method
